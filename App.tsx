0e8d85a238af681807c766849d60576d643187da
unknown <rockstonestudios666@gmail.com>
2023-03-16 17:12:41 -0500

    App

--- App.tsx
+++ App.tsx
@@ -11,24 +11,24 @@ import {
 } from 'react-native';
 import { ApiContext } from './src/context/ApiContext';
 import { NavigationContainer } from '@react-navigation/native';
-// import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Welcome from './src/screens/Welcome';
 import Home from './src/screens/Home';
 
-// const STACK = createNativeStackNavigator();
+const STACK = createNativeStackNavigator();
 
 
 function App(): JSX.Element {
 
   return (
   <ApiContext>
-    {/* <NavigationContainer> */}
-     {/* <STACK.Navigator>
+    <NavigationContainer> 
+      <STACK.Navigator screenOptions={{headerShown : false}}>
         <STACK.Screen name='Welcome' component={Welcome} />
         <STACK.Screen name='Home' component={Home} />
-     </STACK.Navigator> */}
+     </STACK.Navigator>
      <Home/>
-    {/* </NavigationContainer> */}
+     </NavigationContainer> 
    
   </ApiContext>
   );
--- src/screens/Welcome.tsx
+++ src/screens/Welcome.tsx
@@ -1,10 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React,{useEffect} from 'react';
+import {useNavigation} from '@react-navigation/native'
 
 const Welcome = () => {
+  const navigation = useNavigation();
+
+  useEffect(()=>{
+      setTimeout(()=>{
+          navigation.navigate('Home')
+      },2100)
+  },[])
   return (
     <View style={styles.container}>
-      <Text>Q10</Text>
+      <Text style={{fontSize : 35}}>Q10</Text>
     </View>
   )
 }
